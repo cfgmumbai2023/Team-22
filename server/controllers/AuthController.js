ce75f3acb0b4ebc8b6c4a07f88e196c752a2bee4
YoL <77549395+technothron@users.noreply.github.com>
2023-07-08 22:05:47 +0530

    made a controller

---
+++ server/controllers/AuthController.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const auth = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const token = jwt.sign({ userId: user._id }, `${process.env.JWT_SECRET}`, {
+      expiresIn: "1h",
+    });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = auth;
