cbddf13acb38ea3c8f2b95d7900f033fcdfc6e89
arbaaz234 <arbaazghameriya@gmail.com>
2023-07-09 07:17:35 +0530

    update allstudents.js

--- server/controllers/getAllStudents.js
+++ server/controllers/getAllStudents.js
@@ -1,4 +1,5 @@
 import Student from "../models/Student.js";
+import Evaluation from '../models/Evaluation.js';
 const getAllStudent = async (req, res) => {
   try {
     const students = await Student.find({});
@@ -17,7 +18,18 @@ export const addStudent = async (req, res) => {
     age,
     level,
     gender,
-    aadharNo
+    aadharNo,
+    fraction,
+    addition,
+    subtraction,
+    two_words,
+    vocabulary,
+    traditional_modern_game,
+    mumbai_map_topography,
+    lemonSharbat_making,
+    stamping_design,
+    family_member_identification,
+    museumThings_identification
   } = req.body;
   if (!name || !address || !schoolId || !teacherId || !disability || !age || !level || !gender || !aadharNo) {
     return res.status(404).json({ status: false, message: "Not valid data" });
@@ -35,6 +47,22 @@ export const addStudent = async (req, res) => {
   });
   await newStud.save();
   const stud = await Student.find();
+  const stud1 = await Student.findOne({ name })
+  const newEvaluation = Evaluation({
+    studentId: stud1._id,
+    fraction,
+    addition,
+    subtraction,
+    two_words,
+    vocabulary,
+    traditional_modern_game,
+    mumbai_map_topography,
+    lemonSharbat_making,
+    stamping_design,
+    family_member_identification,
+    museumThings_identification
+  });
+  await newEvaluation.save();
   res.status(200).json(stud);
 }
 export default getAllStudent;
